2b38091e5f2b0caa1c2f23b161fa93fdb26feef3
YahyaSaleh123 <yahyasaleh55555@gmail.com>
2025-10-20 19:52:15 +0300

    SERVER

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,22 +1,20 @@
-import express from "express"
 import dotenv from "dotenv";
-dotenv.config();
+import express from "express";
 import { connectToDB } from './DB/db.js';
-import userRouter from "./users/user.router.js";
 import stationRouter from "./stations/station.router.js";
+import userRouter from "./users/user.router.js";
 import vehicleRouter from "./vehicles/vehicle.router.js";
 
+dotenv.config();
 
 const PORT=5500 || process.env.PORT;
-
 const server = express();
+
 server.use(express.json());
 
 server.get('/', (req,res) =>{
     res.send("server is running and connected to DB")
 });
-
-
 
 server.use('/api/user',userRouter)
 server.use('/api/station',stationRouter)
